dc16fc0b49d64b8dfe74d649d0bf451f28107544
Artem Blodov <mr.samy@rambler.ru>
2024-01-02 14:40:10 +0200

    start site

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, contact_view
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', index, name='catalog')
+    path('', index, name='catalog'),
+    path('', contact_view, name='contact')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return [name, phone, message]
+    return render(request, 'catalog/contact.html')
