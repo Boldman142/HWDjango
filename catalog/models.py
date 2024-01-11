84de137eee31ae205c83d464183dc195e6982bb8
Artem Blodov <mr.samy@rambler.ru>
2024-01-11 13:49:57 +0200

    HW

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models import SET_NULL, PROTECT
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    overview = models.TextField(verbose_name='Описание')
+    picture = models.ImageField(upload_to='catalog/', **NULLABLE)
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.SET_DEFAULT, default='Не указано')
+    price = models.IntegerField(verbose_name='Цена за штуку')
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_change = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} {self.price} {self.category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('date_create',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    overview = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from catalog.views import index, contact_view
 
@@ -7,4 +9,4 @@ app_name = 'catalog'
 urlpatterns = [
     path('', index, name='catalog'),
     path('', contact_view, name='contact')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
