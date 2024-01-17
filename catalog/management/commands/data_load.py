132870f6a0b0160d9d4be5cf26104260fe7e8ac8
Artem Blodov <mr.samy@rambler.ru>
2024-01-17 12:37:51 +0200

    Django Dump Load UTF-8

--- catalog/management/commands/data_load.py
+++ catalog/management/commands/data_load.py
@@ -1,50 +1,11 @@
-# import json
-#
-# from django.core.management import BaseCommand
-#
 from catalog.models import Category, Product
-#
-
-# class Command(BaseCommand):
-#     def handle(self, *args, **options):
-#         # Product.objects.all().delete()
-#         # Category.objects.all().delete()
-#
-#         with open('data.json', 'r', encoding='UTF-8') as file:
-#             data = json.load(file)
-#
-#         category = []
-#         product = []
-#
-#         for item in data:
-#             if item['model'] == 'catalog.category':
-#                 new_category = Category(**item['fields'])
-#                 category.append(new_category)
-#
-#         # Category.object.bulk_create(category)
-#         new = Category.objects.all()
-#
-#         # for i in new:
-#         #     print(i.__dict__)
-#         for item in data:
-#             if item['model'] == 'catalog.product':
-#                 pk = item['fields']['category']
-#                 item['fields']['category'] = Category.objects.filter(id=pk)
-#                 product.append(Product(**item['fields']))
-#
-#         # for prod in product:
-#         #     print(Product.object.get(id))
-#
-#
-#         # Product.object.bulk_create(product)
-#
-#         # print('python _Xutf8 manage.py dumpdata catalog -o data.json')
-
 
 import os
 from django.core.management import BaseCommand, call_command
 from psycopg2 import ProgrammingError, IntegrityError
 
+
+# python manage.py dumpdatautf8 catalog --output data.json
 
 class Command(BaseCommand):
 
